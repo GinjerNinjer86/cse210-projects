16180cb94d872adbf52feb9f0da7823ba1d89825
GinjerNinjer86 <cha53w1lk1ns@gmail.com>
2023-04-26 11:20:23 -0700

    Wrote program to determine letter grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter grade percentage: ");
+        string ValueFromUser = Console.ReadLine();
+        int percentage = int.Parse(ValueFromUser);
+        
+        string Letter = "";
+
+        if (percentage >= 90)
+        {
+            Letter = "A";
+        }
+        else if (percentage >= 80)
+        {
+            Letter = "B";
+        }
+        else if (percentage >= 70)
+        {
+            Letter = "C";
+        }
+        else if (percentage >= 60)
+        {
+            Letter = "D";
+        }
+        else
+        {
+            Letter = "F";
+        }
+
+        Console.WriteLine($"Your Letter Grade is: {Letter}");
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
