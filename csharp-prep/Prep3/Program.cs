6df714e1cc00f63819aa60ed9c5b063b40cdd423
GinjerNinjer86 <cha53w1lk1ns@gmail.com>
2023-04-26 11:34:03 -0700

    Created number guessing game

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int rightNumber = randomGenerator.Next(1, 101);
+
+        int UserGuess = -1;
+
+        // We could also use a do-while loop here...
+        while (UserGuess != rightNumber)
+        {
+            Console.Write("What is your guess? ");
+            UserGuess = int.Parse(Console.ReadLine());
+
+            if (rightNumber > UserGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (rightNumber < UserGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+
+        }                    
     }
 }
